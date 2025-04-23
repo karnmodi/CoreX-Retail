e79013ffeaf42bc4c0d3ca682baf589316b9d3f1
Karan Falgun Modi <84740993+karnmodi@users.noreply.github.com>
2025-04-24 00:39:05 +0100

    Index Chnaged

--- corex-retail-backend/index.js
+++ corex-retail-backend/index.js
@@ -59,11 +59,13 @@ async function initializeApp() {
     }
 
     // Start server
-    app.listen(PORT, () => {
-      console.log(`✅ Server is running on http://localhost:${PORT}`);
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
       console.log("✅ Firebase services are connected");
-      
+
     });
+
+
   } catch (error) {
     console.error('Application initialization failed:', error);
     console.error('Detailed error:', error);
